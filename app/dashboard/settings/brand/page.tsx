6d230ea7ccaf41354e08ede8ea0259679bb45d0c
Yugank Bansal <yugankbansal.00001276033.aesl@gmail.com>
2024-05-12 23:31:07 +0530

    refactor: back button added in settings page

--- app/dashboard/settings/brand/page.tsx
+++ app/dashboard/settings/brand/page.tsx
@@ -5,6 +5,8 @@ import ProfileSetting from "@/components/dashboard/settings/ProfileSetting";
 import PaymentSetting from "@/components/dashboard/settings/PaymentSetting";
 import NotificationSetting from "@/components/dashboard/settings/NotificationSetting";
 import React, { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 function BrandSettings() {
   const [activePage, setActivePage] = useState("ProfilePage");
@@ -29,12 +31,22 @@ function BrandSettings() {
     if (activePage == "NotificationPage") {
       return <NotificationSetting />;
     }
-   
   }
 
   function Layout({ children }: { children: React.ReactNode }) {
     return (
       <div className="w-full flex items-center flex-col gap-6 bg-[#1E2023] min-h-screen py-20">
+        <div className="w-full px-10">
+          <Link href="/dashboard">
+            <Image
+              src="/icons/Arrow.svg"
+              width="100"
+              height="100"
+              className="w-[4%] rounded-full bg-[#00B24F] px-4 py-5 rotate-180 flex justify-start"
+              alt="Ref3r logo"
+            />
+          </Link>
+        </div>
         <div className="w-[80%] flex flex-col gap-16">
           <div className="text-white text-6xl font-semibold">Settings</div>
           <SettingsNavBar
--- app/dashboard/settings/influencer/page.tsx
+++ app/dashboard/settings/influencer/page.tsx
@@ -6,6 +6,8 @@ import InfProfileSettings from "@/components/dashboard/settings/influencers/InfP
 import InfPaymentSettings from "@/components/dashboard/settings/influencers/InfPaymentSettings";
 import InfNotificationSettings from "@/components/dashboard/settings/influencers/InfNotificationSettings";
 import ReferalCode from "@/components/dashboard/ReferalCode";
+import Image from "next/image";
+import Link from "next/link";
 
 function InfluencerSettings() {
   const [activePage, setActivePage] = useState("ProfilePage");
@@ -35,15 +37,27 @@ function InfluencerSettings() {
   function Layout({ children }: { children: React.ReactNode }) {
     return (
       <div className="w-full flex items-center flex-col gap-6 bg-[#1E2023] min-h-screen py-20">
+        <div className="w-full px-10">
+          <Link href="/dashboard">
+            <Image
+              src="/icons/Arrow.svg"
+              width="100"
+              height="100"
+              className="w-[4%] rounded-full bg-[#00B24F] px-4 py-5 rotate-180 flex justify-start"
+              alt="Ref3r logo"
+            />
+          </Link>
+        </div>
         <div className="w-[80%] flex flex-col gap-16">
-          <div className="text-white text-6xl font-semibold">Settings</div>
+          <div className="text-white text-6xl font-semibold">
+            <p>Settings</p>
+          </div>
           <InfluencersNavbar
             setActivePage={setActivePage}
             activePage={activePage}
           />
           {children}
         </div>
-  
       </div>
     );
   }
